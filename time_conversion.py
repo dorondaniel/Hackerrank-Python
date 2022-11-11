5c0b9efd5fa0143b6dd715a7d360a74ad8d28789
Doron.N.C.Daniel <76425579+dorondaniel@users.noreply.github.com>
2022-11-11 22:26:13 +0530

    Update time_conversion.py

--- time_conversion.py
+++ time_conversion.py
@@ -8,16 +8,16 @@ import sys
 
 def timeConversion(s):
     # Write your code here
-    if s[-2:] == "AM":
-        if s[:2] == "12":
-            return "00"+s[2:-2]
+    if s[-2:] == "AM": # checks if it is AM
+        if s[:2] == "12": 
+            return "00"+s[2:-2] # if 12 it removes 12 and appends 00
         else:
-            return s[:-2]
+            return s[:-2] # else it is printed without the suffix
     else:
-        hr = int(s[:2])
+        hr = int(s[:2]) # makes first two elements as integer
         if (hr < 12):
-            hr += 12
-        return str(hr) + s[2:-2]
+            hr += 12 # if it is less then 12 then 12 is added to it
+        return str(hr) + s[2:-2] # it is reverted back to string and returned along with elements eliminating first two as well as the suffix
         
 
 if __name__ == '__main__':
