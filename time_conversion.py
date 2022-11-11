7e5fccc9b7efe9dcd17bb59bfd808c18e60dcb77
Doron.N.C.Daniel <76425579+dorondaniel@users.noreply.github.com>
2022-11-11 22:22:17 +0530

    Update time_conversion.py

--- time_conversion.py
+++ time_conversion.py
@@ -10,7 +10,7 @@ def timeConversion(s):
     # Write your code here
     if s[-2:] == "AM":
         if s[:2] == "12":
-            return "00"+s[:-2]
+            return "00"+s[2:-2]
         else:
             return s[:-2]
     else:
